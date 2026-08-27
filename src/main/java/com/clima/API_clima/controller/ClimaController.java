16961a8d8a3327b149f2edc367781f5d9710bc20
MateusAndrade-1809 <mateusandrademotta@gmail.com>
2026-08-27 18:37:55 -0300

    Adiciona service inicial de clima

--- src/main/java/com/clima/API_clima/controller/ClimaController.java
+++ src/main/java/com/clima/API_clima/controller/ClimaController.java
@@ -1,13 +1,20 @@
 package com.clima.API_clima.controller;
 
+import com.clima.API_clima.service.ClimaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClimaController {
 
+	private final ClimaService climaService;
+
+	public ClimaController(ClimaService climaService) {
+		this.climaService = climaService;
+	}
+
 	@GetMapping("/clima")
 	public String testarApi() {
-		return "API de clima funcionando";
+		return climaService.testarApi();
 	}
 }
---
+++ src/main/java/com/clima/API_clima/service/ClimaService.java
@@ -0,0 +1,11 @@
+package com.clima.API_clima.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClimaService {
+
+	public String testarApi() {
+		return "API de clima funcionando";
+	}
+}
